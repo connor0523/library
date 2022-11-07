0a2b9a28384f18129b996e5d42de04deac20a4fc
Connor <connorce0523@gmail.com>
2022-11-07 15:10:23 -0500

    finished library

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,4 +1,3 @@
-import { faBookOpen, faTags } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import Highlight from "./ui/Highlight";
